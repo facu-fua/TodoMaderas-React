d0879428c720074d25772ab3b6b9d7cfed146c13
Facundo Fajardo <facu_232@hotmail.com>
2022-09-09 20:13:42 -0300

    Se agrego un loading para esperar el fetch y un ternario para esperar el mismo

--- src/containers/itemDetailContainer.jsx
+++ src/containers/itemDetailContainer.jsx
@@ -5,12 +5,14 @@ import ItemDetail from "../components/itemDetail"
 
 const ItemDetailContainer = () =>{
     const [itemDetail, setItem] = useState([])
+    const [loading, setLoading] = useState(false)
+
     const itemFetch = async () =>{
         try {
             const response = await fetch("https://api.mercadolibre.com/sites/MLA/search?q=silla%20madera");
             const data = await response.json();
             setItem(data.results)
-            console.log(data.results)
+            setLoading(true)
         } catch (error) {
             console.log(error)
         }
@@ -20,11 +22,13 @@ const ItemDetailContainer = () =>{
         itemFetch()
     },[]);
 
-    return(
-        <div>
-            <ItemDetail item={itemDetail}/>
-        </div>
-    )
+        if(loading){
+            return(
+                <div>
+                    <ItemDetail item={itemDetail}/>
+                </div>
+            )
+        }
 };
 
 export default ItemDetailContainer;
